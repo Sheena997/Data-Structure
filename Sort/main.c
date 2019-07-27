8db299dbd8eb11f7cffffa86aada8b5295dfa397
Sheena爽 <18365368008@163.com>
2019-07-27 16:49:45 +0800

    施爽：于2019年7月29日修改27日上传的常见的八种排序算法的代码。

---
+++ Sort/main.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+int main()
+{
+	InsertSortTest();
+	ShellSortTest();
+	SelectSortTest();
+	HeapSortTest();
+	BubbleSortTest();
+	QuickSortTest1();
+	QuickSortTest2();
+	QuickSortTest3();
+	MergeSortTest();
+	MergeSortTest2();
+	CountSortTest();
+	system("pause");
+	return 0;
+}
