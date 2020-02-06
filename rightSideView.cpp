14d265a3686d69641ed8eed7d749eb55c78b2740
Sheena爽 <18365368008@163.com>
2020-02-06 16:39:20 +0800

    施爽：于2020年2月6日上传的二叉树的右视图(每下跳一层时,第一个右节点摘下来)的代码。

---
+++ rightSideView.cpp
@@ -0,0 +1,46 @@
+/*
+给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+示例:
+输入: [1,2,3,null,5,null,4]
+输出: [1, 3, 4]
+解释:
+
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-right-side-view
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        
+        search(root, 1, res);
+        
+        return res;
+    }
+    
+    void search(TreeNode* root, int k, vector<int>& res)
+    {
+        if(root == NULL)
+            return;
+        
+        if(k > res.size())
+            res.push_back(root->val);
+        search(root->right, k + 1, res);
+        search(root->left, k + 1, res);
+    }
+};
